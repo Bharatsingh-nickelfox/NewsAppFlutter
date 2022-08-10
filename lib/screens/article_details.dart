b16c6c26f81d82fbf22c8c01097d2c0f226e6f73
Bharatsingh-nickelfox <bharat.singh@nickelfox.com>
2022-08-10 11:36:22 +0530

    feat(ArticlesDetails):add statefulwidget for webview

---
+++ lib/screens/article_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArticleDetailsWebView extends StatefulWidget {
+  const ArticleDetailsWebView({Key? key}) : super(key: key);
+
+  @override
+  State<ArticleDetailsWebView> createState() => _ArticleDetailsWebViewState();
+}
+
+class _ArticleDetailsWebViewState extends State<ArticleDetailsWebView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
