06e12036aa76a04aba6191c532ac4b9dd6a2e8ed
Bharatsingh-nickelfox <bharat.singh@nickelfox.com>
2022-08-10 11:43:35 +0530

    feat(ArticleDetails):integrate webview

--- lib/screens/article_details.dart
+++ lib/screens/article_details.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleDetailsWebView extends StatefulWidget {
-  const ArticleDetailsWebView({Key? key}) : super(key: key);
+  final String url;
+
+  const ArticleDetailsWebView({required this.url, Key? key}) : super(key: key);
 
   @override
   State<ArticleDetailsWebView> createState() => _ArticleDetailsWebViewState();
@@ -9,7 +14,15 @@ class ArticleDetailsWebView extends StatefulWidget {
 
 class _ArticleDetailsWebViewState extends State<ArticleDetailsWebView> {
   @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return WebView(
+      initialUrl: widget.url,
+    );
   }
 }
